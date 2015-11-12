8b31d5ec7fcea33dbabc7731528292cb86f7bf2c
Matthias Bolte <matthias@tinkerforge.com>
2015-11-12 15:22:09 +0100

    matlab: Fix callback handling for R2014a
    http://undocumentedmatlab.com/blog/matlab-callbacks-for-java-events-in-r2014a

--- software/examples/matlab/matlab_example_callback.m
+++ software/examples/matlab/matlab_example_callback.m
@@ -7,7 +7,7 @@ function matlab_example_callback()
     UID = 'XYZ'; % Change to your UID
 
     ipcon = IPConnection(); % Create IP connection
-    b = BrickletBarometer(UID, ipcon); % Create device object
+    b = handle(BrickletBarometer(UID, ipcon), 'CallbackProperties'); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
