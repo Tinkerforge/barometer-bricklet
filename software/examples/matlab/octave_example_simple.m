1d37da42017e504595c4c94592cda09c4787dba4
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:04:18 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_simple.m
+++ software/examples/matlab/octave_example_simple.m
@@ -5,8 +5,8 @@ function octave_example_simple()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your Barometer Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    b = java_new("com.tinkerforge.BrickletBarometer", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    b = javaObject("com.tinkerforge.BrickletBarometer", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
